74d9297ef0c51a976d727a426bd9c1ead0ba7e48
mmatejka <michal.matejka@blackboard.com>
2020-08-03 18:10:16 +0200

    Implemented argument parsing based on the specification.

--- log_filter_util/log_filter_util.py
+++ log_filter_util/log_filter_util.py
@@ -1,3 +1,50 @@
+import argparse
+
+
+def argument_parse():
+    parser = argparse.ArgumentParser(
+        usage='%(prog)s [OPTION]... [FILE]',
+    )
+
+    exclusive_args = parser.add_mutually_exclusive_group()
+    exclusive_args.add_argument(
+        '-f', '--first',
+        metavar='NUM',
+        type=int,
+        help='Print first NUM lines'
+    )
+    exclusive_args.add_argument(
+        '-l', '--last',
+        metavar='NUM',
+        type=int,
+        help='Print last NUM lines'
+    )
+
+    parser.add_argument(
+        'FILE',
+        type=argparse.FileType('r'),
+        nargs='?',
+        help='Filename to use. If missing, data is read from STDOUT'
+    )
+    parser.add_argument(
+        '-t', '--timestamps',
+        action='store_true',
+        help='Print lines that contain a timestamp in HH:MM:SS format'
+    )
+    parser.add_argument(
+        '-i', '--ipv4',
+        action='store_true',
+        help='Print lines that contain an IPv4 address, matching IPs are highlighted'
+    )
+    parser.add_argument(
+        '-I', '--ipv6',
+        action='store_true',
+        help='Print lines that contain an IPv6 address (standard notation), matching IPs are highlighted'
+    )
+
+    args = parser.parse_args()
+    return args
+
 
 def print_results(lines: list):
     for line in lines:
@@ -5,6 +52,7 @@ def print_results(lines: list):
 
 
 def main():
+    argument_parse()
     print_results(['test'])
 
 
